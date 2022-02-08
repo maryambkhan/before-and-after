58d48e8cf199db30fd3aa696cef7913b975d9e00
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-02-08 15:03:02 -0700

    before and after of boolean

---
+++ js/boolean.js
@@ -0,0 +1,11 @@
+let favorite = "friends" ;
+    favorite = "modrenFamily";
+
+if (favorite){
+  console.log("friends");
+}
+else {
+console.log("modrenfamily");
+}
+console.log(true);
+console.log(!false);
--- js/string.js
+++ js/string.js
@@ -3,7 +3,7 @@ const  myFirstName = "Maryam";
 const myLastName = "Khan";
 
 const printString = function(name,lastname) {
-  console.log(name, lastname);
+  
  console.log(`my first name is ${myFirstName}  and my last name is ${myLastName}.`);
 }
 printString(myFirstName, myLastName);
