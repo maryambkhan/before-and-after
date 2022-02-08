eafbf823b2b8c5e411fbe69155c38b7a5b9e92c3
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-02-08 14:19:52 -0700

    array befor and after

---
+++ js/array.js
@@ -0,0 +1,14 @@
+// before
+// const listOfNumbers = [1, 2, 3, 4, 20, 13];
+
+// const printArray = function(list){
+//   console.log(list);
+// }
+// printArray(listOfNumbers);
+
+// after
+const arraysLength = [21, 54, 78 ,56, 0];
+const printLength = function(length){
+  console.log(length);
+}
+printLength(arraysLength.length);
